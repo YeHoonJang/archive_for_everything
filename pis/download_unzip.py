948889e995994ee7bfe19a93c9c0540e0251322c
YehoonJang <jangyh0420@gmail.com>
2017-07-25 14:59:17 +0900

    commit the toy code

--- pis/download_unzip.py
+++ pis/download_unzip.py
@@ -27,14 +27,16 @@ soup = BeautifulSoup(html, 'html.parser')
 my_url = soup.select('body > a')
 print("Start Downloading...\n")
 
-file_list = []
+zipfile_list = []
 
 for url in my_url[:2]:
     file_name = url.get('href').split('/')[-1]
-    file_list.append(file_name)
-    
+    #ipg150106.zip
+    zipfile_list.append(file_name)
+    #['ipg150106.zip']
+
     urllib.request.urlretrieve(url.get('href'), file_name)
-    print("Downloading '"+file_name+"' is done\n")
+    print("Downloading '" + file_name + "' is done\n")
 
 
 # In[7]:
@@ -45,43 +47,55 @@ if not os.path.isdir("C:/workspace/code_for_study/pis/patent_file/"):
     os.mkdir("C:/workspace/code_for_study/pis/patent_file/")
 
 unzipped_list = []
+pd_list = []
 
-for i in file_list:
-    
+for i in zipfile_list:
+
     file_path = "C:\workspace\code_for_study\pis" + "\\" + str(i)
-    
+    #C:\workspace\code_for_study\pis\ipg150106.zip
+
     unzipped_name = i.split('.')[0]
+    #ipg150106
     pd_name = unzipped_name[-6:]
-    pd_path = "C:/workspace/code_for_study/pis/patent_file/" + pd_name
-    
+    #150106
+    pd_list.append(pd_name)
+    #['150106']
+    pd_path = "C:\workspace\code_for_study\pis\patent_file\\" + pd_name
+    #C:\workspace\code_for_study\pis\patent_file\150106
+
     if not os.path.isdir(pd_path):
         os.mkdir(pd_path)
-    
+
     unzipped_file = unzipped_name + ".xml"
-    unzipped_list.append(unzipped_file)
-    
-    full_patent = pd_path + "/" + str(pd_name+".xml")
+    #150106.xml
+    # unzipped_list.append(unzipped_file)
+    #['150106.xml']
+
+    full_patent = pd_path + "\\" + unzipped_file
+    #C:\workspace\code_for_study\pis\patent_file\150106\150106.xml
 
 #     unzipped_file_path = "C:/workspace/code_for_study/pis/patent_file"
-    
+
     patent_zip = zipfile.ZipFile(file_path)
     patent_zip.extract(unzipped_file, pd_path)
-    
+
     patent_zip.close()
 
 
 # In[19]:
 
-patent_text_list = []
+# patent_text_list = []
+#
+# for i in pd_list:
+#     patent_text = "C:\workspace\code_for_study\pis\patent_file\\" + str(i)
+#     #C:\workspace\code_for_study\pis\patent_file\150106
+#
+# #     patent_file_list.append(patent_text)
 
-for i in unzipped_list:
-    patent_text = "C:/workspace/code_for_study/pis/patent_file" +"\\" + str(i)
-#     patent_file_list.append(patent_text)
-    
 
-    f = open(patent_text, "r")
+    f = open(full_patent, "r")
     patent = ''
-    
+
     print("1")
     while True:
         line = f.readline()
@@ -89,14 +103,14 @@ for i in unzipped_list:
         if not line: break
     f.close()
     print("2")
-    
+
     start_index = [i.start() for i in re.finditer("""\<\?xml version="1.0" encoding="UTF-8"\?>""", patent)]
     start_index.append(len(patent)+1)
-    
+
     text_list =[]
     for start in range(1, len(start_index)):
         text_list.append(patent[start_index[start-1]:start_index[start]])
-        
+
     for i in range(len(text_list)):
         patent_file = "patent_" + str(i) + ".xml"
         with open(patent_file, "w") as f:
@@ -106,6 +120,3 @@ for i in unzipped_list:
 
 
 # In[ ]:
-
-
-
