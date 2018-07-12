0f2a04d7f46796dbb57c62b6a92420354a992962
YeHoonJang <jangyh0420@gmail.com>
2018-07-12 17:28:01 +0900

    update

--- ini_soft/my_video_list_modified.php
+++ ini_soft/my_video_list_modified.php
@@ -1,50 +1,43 @@
 [insert_php]
-
-$current_user = wp_get_current_user();
-$current_id = $current_user->ID;
-
-global $wpdb;
-$result = $wpdb->get_results ( "SELECT * FROM wp_uploaded_video  WHERE author =" . $current_id );
+$db = new PDO('mysql:host=localhost;dbname=wordpress;charset=utf8', 'root', 'ini6223');
+foreach($_COOKIE as $key => $value)
+{
+  if(preg_match('/^wordpress_logged_/', $key))
+  {
+   global $loggin_cookie;
+   $loggin_cookie =  $value;
+  }
+}
+$cookie_info = explode("|", $loggin_cookie);
+$username =  $cookie_info[0];
+$stmt = $db->prepare("SELECT ID FROM wp_users WHERE user_login=:user_name") ;
+$stmt->execute(array(':user_name' =>  $username));
+$user = $stmt->fetch();
+$user_id = ($user["ID"]);
+$stmt = $db->prepare("SELECT * FROM wp_uploaded_video WHERE author =:user_id AND NOT status ='delete'");
+$stmt->execute(array(':user_id' =>  $user_id));
+$result = $stmt->fetchAll();
+$row_count = $stmt->rowCount();
+echo $row_count.' rows selected';
 
 echo '<table>';
-  echo '<tr>';
-    echo '<th> video_id </th>';
-    echo '<th> title </th>';
-    echo '<th> path </th>';
-    echo '<th> size </th>';
-    echo '<th> author </th>';
-    echo '<th> uploaded_time </th>';
-    echo '<th> type </th>';
-    echo '<th> delete </th>';
-    echo '</tr>';
-    foreach ( $result as $print )   {
-      echo '<tr>';
-      echo '<td>' . $print->video_id.'</td>';
-      $video_id = $print->video_id;
-      echo '<td>' . $print->title.'</td>';
-      echo '<td>' . $print->path.'</td>';
-      echo '<td>' . $print->size.'</td>';
-      echo '<td>' . $print->author.'</td>';
-      $author = $print->author
-      echo '<td>' . $print->uploaded_time.'</td>';
-      echo '<td>' . $print->type.'</td>';
-      echo '<td><button type="button" id="delete_video" onclick="delete_video(".$author.",".$video_id.")" name="delete_video">delete</button></td>';
-      echo '</tr>';
-
-    }
+ echo '<tr>';
+   echo '<td> title </td>';
+   echo '<td> size </td>';
+   echo '<td> uploaded_time </td>';
+   echo '<td> type </td>';
+   echo '<td> delete </td>';
+   echo '</tr>';
+foreach ($result as $key => $val )   {
+     $video_id = $val[0];
+     $title = $val[1];
+     echo '<tr>';
+     echo '<td>' . $val[1].'</td>';
+     echo '<td>' . $val[3].'</td>';
+     echo '<td>' . $val[5].'</td>';
+     echo '<td>' . $val[6].'</td>';
+     echo '<td><a name="'.$video_id.'" id=btn_modal  href="http://192.168.10.37/update_video_status.php?video_id='.$video_id.'">delete</a></td>';
+     echo '</tr>';
+   }
 echo '</table>';
 [/insert_php]
-
-<script>
-function selete_video(author, video_id){
-  $.ajax({
-    type:"POST",
-    url:"http://localhost/delete_video.php",
-    data:"author="+author+"&video_id="+video_id,
-    cache:false,
-    success: function(result) {
-      alert(result);
-    }
-  });
-}
-</script>
