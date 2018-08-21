11d380770f935b090bdf943eb3b8ddf9de6b2612
YeHoonJang <jangyh0420@gmail.com>
2018-08-21 18:34:04 +0900

    8/22 to do

--- ini_soft/django/ini_final_project/ini_final_project/settings.py
+++ ini_soft/django/ini_final_project/ini_final_project/settings.py
@@ -55,7 +55,6 @@ ROOT_URLCONF = 'ini_final_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
---
+++ ini_soft/django/ini_final_project/relocations/forms.py
@@ -0,0 +1,6 @@
+from django import froms
+
+#파일업로드
+class UploadFileForm(forms.Form):
+
+#회원가입
