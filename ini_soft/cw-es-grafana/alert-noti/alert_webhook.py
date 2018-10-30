57fe8a53bdef0160ad2f143eb1d2cd2ae40cbc20
YeHoonJang <jangyh0420@gmail.com>
2018-10-30 18:57:20 +0900

    commit grafana auto alert

--- ini_soft/cw-es-grafana/alert-noti/alert_webhook.py
+++ ini_soft/cw-es-grafana/alert-noti/alert_webhook.py
@@ -5,39 +5,57 @@ import logging
 import requests
 import urllib
 
+import time
+import atexit
+from apscheduler.schedulers.background import BackgroundScheduler
+import schedule
+
+
 app = Flask(__name__)
+
+# def print_date_time():
+#     print(time.strftime("%A, %d. %B %Y %I:%M:%S %p"))
+#
+# def scheduler():
+#     scheduler = BackgroundScheduler()
+#     scheduler.add_job(func=print_date_time, trigger="interval", seconds=30)
+#     scheduler.start()
+#
+#     # Shut down the scheduler when exiting the app
+#
+#     atexit.register(lambda: scheduler.shutdown())
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
 
-def save_image(url):
-    reqeust = urllib.request.Reqeust(url,
-    headers={'Authorization':'Bearer eyJrIjoic09hcXlTY1F1eGo4OGpKb0VqdXh5OGVuVlFCWlQ1bXAiLCJuIjoidGVzdCIsImlkIjoxfQ=='})
-    page = urllib.request.urlopen(request)
-
-    with open('somefile.png','wb') as f:
-        f.write(page.read())
-        # f.open()
-
-    return 'somefile.png'
+    return "hi"
 
 @app.route('/webhook', methods=['POST, PUT'])
 def webhook():
-    url = r'http://13.231.223.137:3000/render/d-solo/4dgCpYbmz/ls-monit-v1-0?orgId=1&from=1540757160583&to=1540778760583&panelId=8&width=1000&height=500&tz=Asia%2FSeoul'
-    file_name = save_image(url)
-    f = open(file_name, 'r+')
-    f.read()
-    f.close()
-    # f = open('somefile.png', 'r+')
-    # pngdata = f.read()
-    # f.close()
-    # if request.method == 'POST':
-    #     print(request.json)
-    #     return '', 200
-    # else:
-    #     abort(400)
+    print(time.strftime("%A, %d. %B %Y %I:%M:%S %p"))
+    return time.strftime("%A, %d. %B %Y %I:%M:%S %p")
 
+# def job():
+#     print("I'm working...")
+#     now = time.gmtime(time.time())
+#     print(now.tm_hour+9, now.tm_min, now.tm_sec)
+
+    # print(schedule.every(1).minutes.do(job))
+    # while True:
+    #     schedule.run_pendeing()
+    #     time.sleep(1)
+
+
+# def job_schedule():
+
+# print("Are you there?")
+# job()
+# schedule.every(1).minutes.do(job)
+# while True:
+#     schedule.run_pendeing()
+#     time.sleep(1)
+
+# job_schedule()
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000', debug=True)
