9202577f9e249bbae9d08ed88085d4f533eb96f3
YeHoonJang <jangyh0420@gmail.com>
2018-11-02 16:20:58 +0900

    add

--- ini_soft/cw-es-grafana/json-server/json_server.py
+++ ini_soft/cw-es-grafana/json-server/json_server.py
@@ -35,5 +35,47 @@ def query():
     print("req['targets'][0]['target']", req['targets'][0]['target'], "\n")
     return jsonify(data)
 
+@app.route('/annotations', methods=['POST'])
+def annotations():
+    req = request.get_json()
+    data = [
+        {
+            "annotation": 'This is the annotation',
+            "time": (convert_to_time_ms(req['range']['from']) +
+                     convert_to_time_ms(req['range']['to'])) / 2,
+            "title": 'Deployment notes',
+            "tags": ['tag1', 'tag2'],
+            "text": 'Hm, something went wrong...'
+        }
+    ]
+    return jsonify(data)
+
+
+@app.route('/tag-keys', methods=['POST'])
+def tag_keys():
+    data = [
+        {"type": "string", "text": "City"},
+        {"type": "string", "text": "Country"}
+    ]
+    return jsonify(data)
+
+
+@app.route('/tag-values', methods=['POST'])
+def tag_values():
+    req = request.get_json()
+    if req['key'] == 'City':
+        return jsonify([
+            {'text': 'Tokyo'},
+            {'text': 'São Paulo'},
+            {'text': 'Jakarta'}
+        ])
+    elif req['key'] == 'Country':
+        return jsonify([
+            {'text': 'China'},
+            {'text': 'India'},
+            {'text': 'United States'}
+        ])
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
