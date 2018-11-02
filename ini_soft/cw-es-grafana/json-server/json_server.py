610dd284a4aa84bdd3a2b641c82706043cc1d898
YeHoonJang <jangyh0420@gmail.com>
2018-11-02 15:54:53 +0900

    add search def

--- ini_soft/cw-es-grafana/json-server/json_server.py
+++ ini_soft/cw-es-grafana/json-server/json_server.py
@@ -7,7 +7,9 @@ app = Flask(__name__)
 def health_check():
     return 'This datasource is healhty.'
 
-
+@app.route('/search', method=['POST'])
+def search():
+    return jsonify(['my_series', 'another_series'])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
