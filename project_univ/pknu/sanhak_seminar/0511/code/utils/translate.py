54d26cd9d1e65f60d0e1b2c3e7311ebfb0993a5c
YeHoonJang <jangyh0420@gmail.com>
2023-05-09 19:18:10 +0900

    delete

--- project_univ/pknu/sanhak_seminar/0511/code/utils/translate.py
+++
@@ -1,5 +0,0 @@
-ERROR_MESSAGE = "GOOGLE ERROR MESSAGE"
-
-def translate_text(text, translater, src='en', dest='de'):
-    result = translater.translate(text, src=src, dest=dest)
-    return result.text
